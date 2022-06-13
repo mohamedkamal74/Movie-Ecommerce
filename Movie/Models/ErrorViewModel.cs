a061a187e877f9e31c190c721760ffd567bd718b
mohamed kamal <mkahf5103@gmail.com>
2022-06-13 20:44:44 +0400

    Add project files.

---
+++ Movie/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace Movie.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
