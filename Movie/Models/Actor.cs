6cb2473317b08065f3c4cca3b00ce627780b5512
mohamed kamal <mkahf5103@gmail.com>
2022-06-14 18:17:38 +0400

    Add Actors  index view

--- Movie/Models/Actor.cs
+++ Movie/Models/Actor.cs
@@ -7,8 +7,13 @@ namespace Movie.Models
     {
         [Key]
         public int Id { get; set; }
+        [Display(Name ="Profile Picture URL")]
         public string Profilepictureurl { get; set; }
+        [Display(Name = "Full Name")]
+
         public string FullName { get; set; }
+        [Display(Name = "Biography")]
+
         public string Bio { get; set; }
         public List< Actor_Movie> Actors_Movies { get; set; }
     }
