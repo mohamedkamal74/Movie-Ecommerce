e71a9216dd08ad1ff7476de8d31531d0b973537c
mohamed kamal <mkahf5103@gmail.com>
2022-06-17 19:17:15 +0400

    Add action and view to cinemas controller

--- Movie/Controllers/CinemasController.cs
+++ Movie/Controllers/CinemasController.cs
@@ -1,11 +1,13 @@
 ﻿using Microsoft.AspNetCore.Mvc;
 using Movie.Data;
+using Movie.Models;
 using System.Linq;
 
 namespace Movie_Ecommerce.Controllers
 {
     public class CinemasController : Controller
     {
+        
         private readonly ApplicationDbContext _context;
 
         public CinemasController(ApplicationDbContext context)
@@ -14,7 +16,84 @@ namespace Movie_Ecommerce.Controllers
         }
         public IActionResult Index()
         {
-            return View(_context.Cinemas.ToList());
+            return View(_context.Cinemas.OrderBy(n => n.Name).ToList());
+        }
+
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public IActionResult Create(Cinema cinema)
+        {
+            if (ModelState.IsValid)
+            {
+                _context.Cinemas.Add(cinema);
+                _context.SaveChanges();
+                return RedirectToAction(nameof(Index));
+            }
+            return View(cinema);
+        }
+
+        public IActionResult Edit(int id)
+        {
+           
+            var cinemaupdate = _context.Cinemas.FirstOrDefault(x => x.Id == id);
+            if (cinemaupdate == null)
+            {
+                return NotFound();
+            }
+            return View(cinemaupdate);
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public IActionResult Edit(Cinema cinema)
+        {
+            if (ModelState.IsValid)
+            {
+                _context.Cinemas.Update(cinema);
+                _context.SaveChanges();
+                return RedirectToAction(nameof(Index));
+            }
+            return View(cinema);
+        }
+
+        public IActionResult Details(int id)
+        {
+
+            var cinema = _context.Cinemas.FirstOrDefault(x => x.Id == id);
+            if (cinema == null)
+            {
+                return NotFound();
+            }
+            return View(cinema);
+        }
+
+        public IActionResult Delete(int id)
+        {
+
+            var cinemaupdate = _context.Cinemas.FirstOrDefault(x => x.Id == id);
+            if (cinemaupdate == null)
+            {
+                return NotFound();
+            }
+            return View(cinemaupdate);
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public IActionResult Delete(Cinema cinema)
+        {
+           if(cinema != null)
+            {
+                _context.Cinemas.Remove(cinema);
+                _context.SaveChanges();
+                RedirectToAction(nameof(Index));
+            }
+            return View(cinema);
         }
     }
 }
