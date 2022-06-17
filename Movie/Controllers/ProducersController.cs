34f4ff09170caf46f1bfe51f0ddbb6c70eae3479
mohamed kamal <mkahf5103@gmail.com>
2022-06-17 20:09:27 +0400

    add actions and views to producer controller

--- Movie/Controllers/ProducersController.cs
+++ Movie/Controllers/ProducersController.cs
@@ -1,5 +1,6 @@
 ﻿using Microsoft.AspNetCore.Mvc;
 using Movie.Data;
+using Movie.Models;
 using System.Linq;
 
 namespace Movie_Ecommerce.Controllers
@@ -15,6 +16,84 @@ namespace Movie_Ecommerce.Controllers
         public IActionResult Index()
         {
             return View(_context.Producers.ToList());
+        }
+
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public IActionResult Create(Producer model)
+        {
+            if (ModelState.IsValid)
+            {
+                _context.Producers.Add(model);
+                _context.SaveChanges();
+                return RedirectToAction(nameof(Index));
+            }
+            return View(model);
+        }
+
+        public IActionResult Edit(int id)
+        {
+
+            var Producerupdate = _context.Producers.FirstOrDefault(x => x.Id == id);
+            if (Producerupdate == null)
+            {
+                return NotFound();
+            }
+            return View(Producerupdate);
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public IActionResult Edit(Producer model)
+        {
+            if (ModelState.IsValid)
+            {
+                _context.Producers.Update(model);
+                _context.SaveChanges();
+                return RedirectToAction(nameof(Index));
+            }
+            return View(model);
+        }
+
+        public IActionResult Details(int id)
+        {
+
+            var producer = _context.Producers.FirstOrDefault(x => x.Id == id);
+            if (producer == null)
+            {
+                return NotFound();
+            }
+            return View(producer);
+        }
+
+        public IActionResult Delete(int id)
+        {
+
+            var producer = _context.Producers.FirstOrDefault(x => x.Id == id);
+            if (producer == null)
+            {
+                return NotFound();
+            }
+            return View(producer);
+        }
+
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public IActionResult DeleteConfirm(int id)
+        {
+            var producer = _context.Producers.FirstOrDefault(x => x.Id == id);
+            if (producer == null)
+            {
+                return NotFound();
+            }
+            _context.Producers.Remove(producer);
+            _context.SaveChanges();
+            return RedirectToAction(nameof(Index));
         }
     }
 }
